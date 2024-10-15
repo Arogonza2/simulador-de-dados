594d7629876f185d21bf5cceaff71bc3b2ed8a5a
Arogonza2 <piponso2022@outlook.com>
2024-10-15 23:04:57 +0200

    ejercicios

---
+++ Ejercicios Mouredev/scripts.py
@@ -0,0 +1,25 @@
+# Cree un programa que , al recicbipr como datos dos numeros enteros, calcule ela suma, resta y multiplicacion de dichos numeros 
+"""
+n1 =float(input("Introduce el primer numero:"))
+n2 =float(input("Introduce el segundo numero:"))
+
+suma = n1 +n2
+resta = n1- n2
+multi = n1 * n2 
+
+print("La suma es:", suma)
+print("La resta es :", resta)
+print("La multiplicacion es :", multi )
+"""
+
+"""
+ En la olimpiada 
+"""
+
+num = input(" Ingrase un numero entero de cuatro digitos: ")
+
+
+print(num[0])
+print(num[1])
+print(num[2])
+print(num[3])
