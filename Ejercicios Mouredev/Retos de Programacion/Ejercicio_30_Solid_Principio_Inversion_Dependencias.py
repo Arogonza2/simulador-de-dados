4366d418aa8ec77a61bcf1232726a43351edc06b
Arogonza2 <piponso2022@outlook.com>
2024-12-05 22:41:39 +0100

    Ejercicio

---
+++ Ejercicios Mouredev/Retos de Programacion/Ejercicio_30_Solid_Principio_Inversion_Dependencias.py
@@ -0,0 +1,12 @@
+"""
+EJERCICIO:
+ * Explora el "Principio SOLID de Inversión de Dependencias (Dependency Inversion
+ * Principle, DIP)" y crea un ejemplo simple donde se muestre su funcionamiento 
+ * de forma correcta e incorrecta.
+ 
+
+"""
+
+class swich:
+    pass
+
