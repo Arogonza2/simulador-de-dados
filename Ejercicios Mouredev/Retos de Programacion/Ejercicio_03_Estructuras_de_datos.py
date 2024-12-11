af47eb34b8b3679a40aa507a86314a00b66c49cb
Arogonza2 <piponso2022@outlook.com>
2024-12-11 13:55:11 +0100

    Ejercicios

--- Ejercicios Mouredev/Retos de Programacion/Ejercicio_03_Estructuras_de_datos.py
+++ Ejercicios Mouredev/Retos de Programacion/Ejercicio_03_Estructuras_de_datos.py
@@ -68,9 +68,84 @@ print(my_dict)
 
 
 
+"""
+Extra
+"""
 
 
- 
+
+def my_agenda():
+  
+    agenda={}
+    
+    def insert_contact():
+        phone = input("Introduce el telefono de contacto: ")
+        if phone.isdigit() and len(phone) >0 and len(phone) <= 11:
+            agenda[name]= phone
+              
+        else:
+            print(
+                  "Debes introducir un numero de telefono con un maximo de 11 digitos ")
+      
+  
+    while True:
+      
+        print(" ")     
+        print("1. Buscar contacto")
+        print("2. Insertar contacto")
+        print("3. Actualizar contacto")
+        print("4. Eliminar contacto")
+        print("5. Salir")
+      
+      
+        option = input("\nSelecciona una opcion: ")
+      
+        match option:
+          
+            case "1":
+              name = input("Introduce el nombre del contacto a buscar: " )
+              if name in agenda:
+                  print(
+                    f" El numero de telefono de {name} es {agenda[name]}. ")
+              
+              else:
+                  print(f"El contacto {name} no existe. ")
+        
+          
+            case "2":
+              name = input("Introduce el nombre del contacto: ")
+              insert_contact()
+                      
+            case "3":
+              name = input("Introduce el nombre del contacto a actualizar: " )
+              if name in agenda:
+                insert_contact()
+                        
+              else:
+                  print(f"El contacto {name} no existe.")
+                        
+      
+            case "4":
+              name = input("Introduce el nombre del contacto a eliminar: " )
+              if name in agenda:
+                del agenda[name]
+              
+              else:
+                  print(f"El contacto {name} no existe.")
+            
+            
+          
+            case "5":
+                print( "Saliendo de la agenda")
+                break
+            
+            
+            case _:
+                print("Opcion no valida, selecciona una opcion valida: ")
+          
+my_agenda()
+      
+     
 
 
 
